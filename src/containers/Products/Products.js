1502fbc965195d5adcfd3b5b075e3b4e01bd3a78
Natty Teshome <nattyteshome30@gmail.com>
2022-11-16 21:45:33 -0600

    initial commit

---
+++ src/containers/Products/Products.js
@@ -0,0 +1,17 @@
+import Product from "../../components/Product/Product";
+
+const Products = (props) => {
+  const products = props.products.map((product) => {
+    return (
+      <Product
+        name={product.name}
+        price={product.price}
+        id={product.id}
+        key={product.id}
+      />
+    );
+  });
+  return products;
+};
+
+export default Products;
